f557dfabf98ca09b64cb0354b1fa29186d86d87f
Andrew <drewbusi123@gmail.com>
2022-11-12 16:08:16 -0500

    Rama/Luis front-end progress

---
+++ Client/Components/OverviewContainer.tsx
@@ -0,0 +1,99 @@
+import * as React from 'react'
+// import React, {FC} from 'react';
+
+const OverviewContainer = () => {
+
+//we will first get a list of containers with metrics 
+const containersArray = [
+    {
+    "BlockIO":"0B / 0B",
+    "CPUPerc":"0.00%",
+    "Container":"3e2836e39536",
+    "ID":"3e2836e39536",
+    "MemPerc":"0.15%",
+    "MemUsage":"11.57MiB / 7.475GiB",
+    "Name":"crazy_tu",
+    "NetIO":"1.08kB / 0B",
+    "PIDs":"17"
+    },
+    {
+        "BlockIO":"0B / 0B",
+        "CPUPerc":"60.23%",
+        "Container":"3e2836e39536",
+        "ID":"3e2836e39536",
+        "MemPerc":"0.15%",
+        "MemUsage":"11.57MiB / 7.475GiB",
+        "Name":"crazy_tu",
+        "NetIO":"1.08kB / 0B",
+        "PIDs":"17"
+    },
+    {
+            "BlockIO":"0B / 0B",
+            "CPUPerc":"90.99%",
+            "Container":"3e2836e39536",
+            "ID":"3e2836e39536",
+            "MemPerc":"0.15%",
+            "MemUsage":"11.57MiB / 7.475GiB",
+            "Name":"crazy_tu",
+            "NetIO":"1.08kB / 0B",
+            "PIDs":"17"
+    },
+    {
+        "BlockIO":"0B / 0B",
+        "CPUPerc":"90.99%",
+        "Container":"3e2836e39536",
+        "ID":"3e2836e39536",
+        "MemPerc":"85.15%",
+        "MemUsage":"11.57MiB / 7.475GiB",
+        "Name":"crazy_tu",
+        "NetIO":"1.08kB / 0B",
+        "PIDs":"17"
+}
+
+]
+
+//if the containers array is emtpy display : You don't have any containers to show
+
+//else :
+
+//we would need useeffect to check run the functionality each time, in order to check which container should go to the top
+
+const containers = [];
+for(let i = 0; i < containersArray.length; i++){
+// we would need to loop through the array of containers
+const element = containersArray[i];
+//string to be reasigned after we check the health of the container
+let health:string;
+//removing everything and leaving only numbers
+const cpu = element.CPUPerc.replace(/\D+/g, '').slice(0, 2);
+const memory = element.CPUPerc.replace(/\D+/g, '').slice(0, 2);
+
+console.log("num", cpu)
+
+
+// if(Number(numberCpu[0] + numberCpu[1]) > 75 || ){
+//     health = 'Bad Shape'
+// }
+
+
+
+containers.push(<div key={i} id={`containerNum${i}`} className='justify-self-center border-4 border-blue-400 rounded-md max-h-[5%] min-h-[70%] min-w-[100%]'>
+<h6>Container Name: {containersArray[i].Name}</h6>  
+<span>Container Health: {} </span>
+</div> )
+ //checking for cpu and memory 
+  //if the cpu or memory are over 65% 
+   //change the border to red
+
+}
+
+ //depending on that we would show the health of the container
+
+
+  return (
+    <>
+    {containers}
+    </>
+  )
+}
+export default OverviewContainer;
--- Client/Containers/DataOverview.tsx
+++ Client/Containers/DataOverview.tsx
@@ -1,7 +1,9 @@
 import * as React from 'react'
+import OverviewContainer from '../Components/OverviewContainer'
 
 export default function DataOverview() {
-  const dataObj = {
+  const containersArray = [
+    {
     "BlockIO":"0B / 0B",
     "CPUPerc":"0.00%",
     "Container":"3e2836e39536",
@@ -11,8 +13,41 @@ export default function DataOverview() {
     "Name":"crazy_tu",
     "NetIO":"1.08kB / 0B",
     "PIDs":"17"
+    },
+    {
+        "BlockIO":"0B / 0B",
+        "CPUPerc":"38%",
+        "Container":"3e2836e39536",
+        "ID":"3e2836e39536",
+        "MemPerc":"0.15%",
+        "MemUsage":"11.57MiB / 7.475GiB",
+        "Name":"crazy_tu",
+        "NetIO":"1.08kB / 0B",
+        "PIDs":"17"
+    },
+    {
+            "BlockIO":"0B / 0B",
+            "CPUPerc":"90%",
+            "Container":"3e2836e39536",
+            "ID":"3e2836e39536",
+            "MemPerc":"0.15%",
+            "MemUsage":"11.57MiB / 7.475GiB",
+            "Name":"crazy_tu",
+            "NetIO":"1.08kB / 0B",
+            "PIDs":"17"
     }
-  return (
-    <div>DataOverview</div>
-  )
+
+]
+
+
+    return (
+      <> 
+      <div className='text-center p-5 max-h-[10%] items-center text-3xl font-bold underlined'>
+      <header className='content-center'>dataoverview</header>
+      </div>
+      <div className='grid overflow-auto h-[50%]'> 
+      {containersArray.length === 0 ? <header> No container to show</header> :  <OverviewContainer/>}
+      </div>
+      </>
+    )
 }
