c909ac948360b15f7cb966fbb19c856697578334
Andrew <drewbusi123@gmail.com>
2022-11-14 15:40:29 -0500

    Smol redux changes

---
+++ Client/store.ts
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+    reducer: {},
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
