ccccf03c05b86163654d939c59ffc2684cddc929
Anna Larouche <annalarouche@Annas-MacBook-Air.local>
2022-11-14 17:00:30 -0800

    trying to add a chart

--- Client/index.js
+++ Client/index.js
@@ -3,15 +3,12 @@ import App from './App';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './style.css';
-import store from './store';
-import { Provider } from 'react-redux';
+//import { Provider } from 'react-redux';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </BrowserRouter>
 );
