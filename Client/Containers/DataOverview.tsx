f5d94c93a39b3a094e7d2459ccd03d24d9ae6882
Luisortzg <88999324+Luisortzg@users.noreply.github.com>
2022-11-15 21:30:31 -0500

    nc

--- Client/Containers/DataOverview.tsx
+++ Client/Containers/DataOverview.tsx
@@ -120,37 +120,39 @@ export default function DataOverview() {
       health = "Bad Shape";
       danger = "border-red-400";
     }
+    if (cpu > 75) notifs.push(`Container ${containersArray[i].Name} has a very high CPU Usage!`)
+    if (memory > 75) notifs.push(`Container ${containersArray[i].Name} has a very high MEM Usage!`)
     containers.push(
       <OverviewContainer
         key={i}
         id={`containerNum${i}`}
         name={containersArray[i].Name}
         health={health}
+        notifs={notifs}
         className={`justify-self-center border-4 ${danger} rounded-md max-h-[5%] min-h-[100%] min-w-[100%] `}
       />
     );
-    if (cpu > 75) notifs.push(`Container ${containersArray[i].Name} has a very high CPU Usage!`)
-    if (memory > 75) notifs.push(`Container ${containersArray[i].Name} has a very high MEM Usage!`)
+
   }
 
-  //is there a way to override the parent contianer with 
-  //grid overflow == no
+  
   
   return (
     <>
       <div className="text-center p-5 max-h-[10%] items-center text-3xl font-bold underlined">
         <header className="content-center">dataoverview</header>
       </div>
-      <div className="grid overflow-auto h-[70%]">
         {containersArray.length === 0 ? (
+            <div className="grid overflow-auto h-[70%]">
           <header> No container to show</header>
+          </div>
         ) : (
           containers
         )}
-      </div>
-      <div className='border-t-4 h-[25%] border-blue-400'>
-        <Notifications notifs={notifs} />
-      </div>
+        <div>
+        <h1 className='font-extrabold text-lg p-1'>Container Notifications</h1>
+          {notifs.map((str:any) => {return <h1>{str}</h1>})}
+        </div>
     </>
   );
 }
