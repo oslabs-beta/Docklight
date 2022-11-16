62cad35b3ff9a1ca5a30febf455025acf2d4849c
Dennis <hdennispark@gmail.com>
2022-11-16 13:16:17 -0500

    homepage notes and linechart bugfixing

--- Client/Components/Homepage.tsx
+++ Client/Components/Homepage.tsx
@@ -5,3 +5,11 @@ export default function Homepage() {
     <div>HOMEPAGE COMPONENT WITH INSTRUCTIONS</div>
   );
 }
+/*
+Hello welcome to Docklight
+This button does this and this button does this
+----  The "Data Overview" page could be retitled to something like "your containers" or "containers"
+Data Overview displays your active and inactive containers.
+  Active containers show a status bar of their relative health, and are ordered top to bottom from least healthy to stable.
+Your Containers shows active containers and their 
+ */
