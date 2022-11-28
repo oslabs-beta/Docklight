afa46e93e4a5a3808d9bf2245dc8daec35fbf283
Andrew <drewbusi123@gmail.com>
2022-11-28 10:35:31 -0500

    Smol Anna Changes

--- server/controllers/ContainerMountController.ts
+++ server/controllers/ContainerMountController.ts
@@ -8,31 +8,31 @@ export const containerMountController = {
     stopContainer: async (req: Request, res: Response, next: NextFunction) => {
         const { ID } = req.body
         try { 
-          const { stdout } = await execProm(`docker stop ${ID}`);
-          console.log(stdout)
-          return next();
+            const { stdout } = await execProm(`docker stop ${ID}`);
+            console.log(stdout)
+            return next();
         }
         catch(err) {
-          return next({
+            return next({
             log: `error ${err} occurred in dockerContainers`,
             message: {err: 'an error occured'}
-          });
+            });
         }
-      },
+    },
 
     startContainer: async (req: Request, res: Response, next: NextFunction) => {
-    const { ID } = req.body
-    try { 
-        const { stdout } = await execProm(`docker start ${ID}`);
-        console.log(stdout)
-        return next();
-    }
-    catch(err) {
-        return next({
-        log: `error ${err} occurred in dockerContainers`,
-        message: {err: 'an error occured'}
-        });
-    }
+        const { ID } = req.body
+        try { 
+            const { stdout } = await execProm(`docker start ${ID}`);
+            console.log(stdout)
+            return next();
+        }
+        catch(err) {
+            return next({
+            log: `error ${err} occurred in dockerContainers`,
+            message: {err: 'an error occured'}
+            });
+        }
     }
 
 }
