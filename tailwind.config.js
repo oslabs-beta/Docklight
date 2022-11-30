1aaa84565e7983199e20f1306790070b3ab9c0a7
Dennis <hdennispark@gmail.com>
2022-11-30 12:01:26 -0500

    adaptive daisyui themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  darkMode: 'class',
+  darkMode: ['class', '[data-theme="dark"]'],
   content: ['./Client/**/*.{js,jsx,ts,tsx}'],
   theme: {  
     extend: {
@@ -19,17 +19,19 @@ module.exports = {
   plugins: [require('daisyui')],
   daisyui: {
     styled: true,
-    themes: ['light', 'dark',
+    themes: ['light', 'dark', 'synthwave', 'cyberpunk', 'autumn', 'dracula', 'lofi', 'valentine','forest', 'night',
       {
         light: {
           ...require('daisyui/src/colors/themes')['[data-theme=light]'],
           primary: '#2696ea',
-          'base-100': '#fffffe',
+          secondary: '#3b82f6',
+          'base-100': '#ffffff',
           'base-200': '#eff6ff'
         },
         dark: {
           ...require('daisyui/src/colors/themes')['[data-theme=dark]'],
           primary: '#2696ea',
+          secondary: '#3b82f6',
         }
       },
     ],
